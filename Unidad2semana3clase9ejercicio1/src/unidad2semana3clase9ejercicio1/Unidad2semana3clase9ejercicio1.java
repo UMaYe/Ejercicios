101b0b1c8c18b0a6713dcdc962c30ee3575b33ce
UMaYe <113459353+UMaYe@users.noreply.github.com>
2022-09-13 15:54:32 -0600

    Add files via upload

---
+++ Unidad2semana3clase9ejercicio1/src/unidad2semana3clase9ejercicio1/Unidad2semana3clase9ejercicio1.java
@@ -0,0 +1,9 @@
+
+package unidad2semana3clase9ejercicio1;
+public class Unidad2semana3clase9ejercicio1 {
+    public static void main(String[] args) {
+      for(int i=100;i>=i;i--)
+          System.out.println(i);
+    }
+    
+}
