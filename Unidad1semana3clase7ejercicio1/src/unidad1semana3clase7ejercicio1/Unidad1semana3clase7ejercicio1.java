dc003c783a2df432e905b6805d38724971c09ec7
UMaYe <113459353+UMaYe@users.noreply.github.com>
2022-09-13 15:37:00 -0600

    Add files via upload

---
+++ Unidad1semana3clase7ejercicio1/src/unidad1semana3clase7ejercicio1/Unidad1semana3clase7ejercicio1.java
@@ -0,0 +1,9 @@
+
+package unidad1semana3clase7ejercicio1;
+
+public class Unidad1semana3clase7ejercicio1 {
+    public static void main(String[] args) {
+        System.out.println("Buenos días estrellitas, la tierra les dice hola");
+    }
+    
+}
