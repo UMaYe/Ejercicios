025982737c1f19658aed22a211db5024e1abb13b
UMaYe <113459353+UMaYe@users.noreply.github.com>
2022-09-13 15:55:52 -0600

    Add files via upload

---
+++ Unidad2semana4clase11ejercicio1/src/unidad2semana4clase11ejercicio1/Unidad2semana4clase11ejercicio1.java
@@ -0,0 +1,11 @@
+
+package unidad2semana4clase11ejercicio1;
+public class Unidad2semana4clase11ejercicio1 {
+    public static void main(String[] args) {
+       String cadena= "";
+       for (int i=1; i<=20; i++){
+           cadena = cadena + "*";
+           System.err.println(cadena);
+    }
+   } 
+}
